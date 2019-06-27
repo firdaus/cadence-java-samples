8dfcdb6da9f389be0dc54f49da55a18685df54b1
Maxim Fateev <maxim@uber.com>
2019-06-27 12:24:02 -0700

    timer time bug

--- src/main/java/com/uber/cadence/samples/hello/HelloQuery.java
+++ src/main/java/com/uber/cadence/samples/hello/HelloQuery.java
@@ -19,13 +19,16 @@ package com.uber.cadence.samples.hello;
 
 import static com.uber.cadence.samples.common.SampleConstants.DOMAIN;
 
+import com.uber.cadence.activity.ActivityMethod;
 import com.uber.cadence.client.WorkflowClient;
 import com.uber.cadence.client.WorkflowOptions;
 import com.uber.cadence.worker.Worker;
-import com.uber.cadence.workflow.QueryMethod;
-import com.uber.cadence.workflow.Workflow;
-import com.uber.cadence.workflow.WorkflowMethod;
+import com.uber.cadence.workflow.*;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /** Demonstrates query capability. Requires a local instance of Cadence server to be running. */
 public class HelloQuery {
@@ -42,10 +45,29 @@ public class HelloQuery {
     String queryGreeting();
   }
 
+  public interface GreetingActivities {
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 60)
+    String composeGreeting(String string);
+  }
+
+  static class GreetingActivitiesImpl implements HelloQuery.GreetingActivities {
+    @Override
+    public String composeGreeting(String string) {
+      try {
+        Thread.sleep(10000);
+      } catch (Exception e) {
+        System.out.println("Exception");
+      }
+      return "greetings: " + string;
+    }
+  }
+
   /** GreetingWorkflow implementation that updates greeting after sleeping for 5 seconds. */
   public static class GreetingWorkflowImpl implements GreetingWorkflow {
 
     private String greeting;
+    private final GreetingActivities activities =
+            Workflow.newActivityStub(GreetingActivities.class);
 
     @Override
     public void createGreeting(String name) {
@@ -54,6 +76,37 @@ public class HelloQuery {
       // and Workflow.currentTimeMillis instead of standard Java ones.
       Workflow.sleep(Duration.ofSeconds(2));
       greeting = "Bye " + name + "!";
+      Promise<String> promiseString1 =
+              Async.function(() -> activities.composeGreeting("1"));
+      Promise<String> promiseString2 =
+              Async.function(
+                      () -> {
+                        return "aString2";
+                      });
+
+      Set<Promise<String>> promiseSet = new HashSet<>();
+      promiseSet.add(promiseString1);
+      promiseSet.add(promiseString2);
+      System.out.println("HERE");
+      Workflow.await(Duration.ofSeconds(30),() -> promiseSet.stream().anyMatch(Promise::isCompleted));
+
+//      If you use below while loop instead of await(), workflow execution completes and behavior is as expected
+//      while (true) {
+//        if (promiseSet.stream().anyMatch(Promise::isCompleted)) {
+//          break;
+//        }
+//        Workflow.sleep(500);
+//        System.out.println("tick");
+//      }
+
+      System.out.println("HERE2");
+      greeting = promiseString1.get();
+      System.out.println("HERE3 " + greeting);
+
+      // Change below line to 21 s, then workflow execution completes
+      Workflow.sleep(Duration.ofSeconds(20));
+      greeting = promiseString2.get();
+      System.out.println("HERE4 " + greeting);
     }
 
     @Override
@@ -67,28 +120,33 @@ public class HelloQuery {
     Worker.Factory factory = new Worker.Factory(DOMAIN);
     Worker worker = factory.newWorker(TASK_LIST);
     worker.registerWorkflowImplementationTypes(GreetingWorkflowImpl.class);
+    worker.registerActivitiesImplementations(new HelloQuery.GreetingActivitiesImpl());
     factory.start();
 
     // Start a workflow execution. Usually this is done from another program.
     WorkflowClient workflowClient = WorkflowClient.newInstance(DOMAIN);
     // Get a workflow stub using the same task list the worker uses.
     WorkflowOptions workflowOptions =
-        new WorkflowOptions.Builder()
-            .setTaskList(TASK_LIST)
-            .setExecutionStartToCloseTimeout(Duration.ofSeconds(30))
-            .build();
+            new WorkflowOptions.Builder()
+                    .setTaskList(TASK_LIST)
+                    .setExecutionStartToCloseTimeout(Duration.ofSeconds(3600))
+                    .build();
     GreetingWorkflow workflow =
-        workflowClient.newWorkflowStub(GreetingWorkflow.class, workflowOptions);
+            workflowClient.newWorkflowStub(GreetingWorkflow.class, workflowOptions);
+
     // Start workflow asynchronously to not use another thread to query.
     WorkflowClient.start(workflow::createGreeting, "World");
     // After start for getGreeting returns, the workflow is guaranteed to be started.
     // So we can send a signal to it using workflow stub.
 
-    System.out.println(workflow.queryGreeting()); // Should print Hello...
+//    System.out.println(workflow.queryGreeting()); // Should print Hello...
     // Note that inside a workflow only WorkflowThread.sleep is allowed. Outside
     // WorkflowThread.sleep is not allowed.
     Thread.sleep(2500);
-    System.out.println(workflow.queryGreeting()); // Should print Bye ...
+//    System.out.println(workflow.queryGreeting()); // Should print Bye ...
+    Thread.sleep(35000);
+//    System.out.println(workflow.queryGreeting());
+    Thread.sleep(600000);
     System.exit(0);
   }
 }
